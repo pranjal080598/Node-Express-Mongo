a0b893305289cc9fdf06ee3643fe241f4977581b
pranjal080598 <pranjal080598@gmail.com>
2020-06-19 12:15:11 +0530

    Express Morgan to serve static files

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public')); //root folder is public from where static files will be retrieved
 
-app.use((req, res, next) => { //'next' parameter is the middleware that express uses 
-  console.log(req.headers);
+app.use((req, res, next) => { //'next' parameter is the middleware that express uses
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');

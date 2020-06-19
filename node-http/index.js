25b67793dffc2cb5f97c571a56d6c4b9917242b9
pranjal080598 <pranjal080598@gmail.com>
2020-06-19 11:12:04 +0530

    Node HTTP Exercise 2

--- node-http/index.js
+++ node-http/index.js
@@ -1,13 +1,47 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+  console.log('Request for ' + req.url + ' by method ' + req.method);
+
+  if (req.method == 'GET') {
+    var fileUrl; //declared varibale
+    if (req.url == '/') fileUrl = '/index.html'; //if no specific url is requested show user index.html
+    else fileUrl = req.url;
+
+    var filePath = path.resolve('./public'+fileUrl); //resolves path to an absolute path that is the file should be within public folder
+    const fileExt = path.extname(filePath); //find the extension of file
+    if (fileExt == '.html') {               //check if extension is html
+      fs.exists(filePath, (exists) => {
+        if (!exists) {                     // if no html file exits
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end('<html><body><h1>Error 404: ' + fileUrl +
+                      ' not found</h1></body></html>');
+          return;
+        }
+        res.statusCode = 200;               //if it exists
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res); //read the contents of the file and pipe it and send the content
+      });
+    }
+    else { //if file extension is not html
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><h1>Error 404: ' + fileUrl +
+              ' not a HTML file</h1></body></html>');
+    }
+  }
+  else { //if request method is post
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><h1>Error 404: ' + req.method +
+              ' not supported</h1></body></html>');
+  }
 })
 
 server.listen(port, hostname, () => {

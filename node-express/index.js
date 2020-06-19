54ae142c43eabb026c95559dec9f8c21b594a9a8
pranjal080598 <pranjal080598@gmail.com>
2020-06-19 18:10:12 +0530

    Express generator

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,7 +13,15 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public')); //root folder is public from where static files will be retrieved
 app.use(bodyParser.json()); //
+
 app.use('/dishes', dishRouter); // any request coming for /dishes will be handled by dishrouter
+app.use('/dishes/:dishId', dishRouter);
+
+app.use('/promotions', promoRouter); // any request coming for /promotions will be handled by dishrouter
+app.use('/promotions/:promoId', promoRouter);
+
+app.use('/leaders', leaderRouter); // any request coming for /promotions will be handled by dishrouter
+app.use('/leaders/:leaderId', leaderRouter);
 
 app.use((req, res, next) => { //'next' parameter is the middleware that express uses
   res.statusCode = 200;

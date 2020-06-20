15e62f68b4e6bee9bdaa83d3b1607d47da19b5bf
pranjal080598 <pranjal080598@gmail.com>
2020-06-20 15:55:03 +0530

    MongoDB Mongoose 1

---
+++ node-mongoose/index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/confused';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save() //saves the dish
+        .then((dish) => { //promise
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+      });

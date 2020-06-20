fdc6c252508a2e03739210e4b16d9df8377e644f
pranjal080598 <pranjal080598@gmail.com>
2020-06-20 13:26:57 +0530

    Node MongoDB 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert'); //for true false values
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'confused';
@@ -11,26 +12,29 @@ MongoClient.connect(url, (err, client) => { //callback function
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"}, //one item to be inserted
-    (err, result) => {
-        assert.equal(err,null);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        console.log("After Insert:\n");
-        console.log(result.ops); //ops is operations that is one operation is performed
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-            console.log("Found:\n");
-            console.log(docs); // print docs
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
 
-                client.close();
+                                client.close();
+                            });
+                        });
+                    });
             });
-        });
-      });
+    });
 
 });

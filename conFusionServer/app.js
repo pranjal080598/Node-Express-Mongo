14dec75f4f3ab3db2b1e8f3e8d85b4a92f20bc8a
pranjal080598 <pranjal080598@gmail.com>
2020-06-21 20:18:30 +0530

    Passport JWT

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -6,7 +6,11 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
-//var authenticate = require('./authenticate');
+var path = require('path');
+//var authenticate = require( path.resolve( __dirname, "conFusionServer/authenticate" ) );
+var authenticate = require('./authenticate.js');
+var config = require('./config');
+const url = config.mongoUrl;
 
 
 var indexRouter = require('./routes/index');
@@ -14,14 +18,6 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('./models/user');
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser()); //session related
-passport.deserializeUser(User.deserializeUser()); //session related
 
 
 
@@ -46,38 +42,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321')); //pass any secret key
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
-
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
+//
+//
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use('/', indexRouter); //welcome to express page
 app.use('/users', usersRouter); //user to login, signup and logout
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      next(err);
-    }
-    else {
-          next();
-    }
-}
-
-app.use(auth);
+// function auth (req, res, next) {
+//     console.log(req.user);
+//
+//     if (!req.user) {
+//       var err = new Error('You are not authenticated!');
+//       err.status = 403;
+//       next(err);
+//     }
+//     else {
+//           next();
+//     }
+// }
+//
+// app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
